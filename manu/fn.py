f193de839808ba2e1bbf3cb24e3c5b8aeebdce64
Abbas Gussenov <abbas.gusenov@pm.me>
2022-08-09 18:19:23 +0600

    Add function get_resource_pack_data

---
+++ manu/fn.py
@@ -0,0 +1,9 @@
+import json
+import os
+
+
+def get_resource_pack_data(game_project_dir_path):
+    resource_pack_file_path = os.path.join(game_project_dir_path, 'resource-pack.json')
+    with open(resource_pack_file_path, 'r') as resource_pack_file:
+        resource_pack_data = json.load(resource_pack_file)
+    return resource_pack_data
