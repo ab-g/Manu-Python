ebbaa7b768548b41fd1f25a7c4e3424083499cad
Abbas Gussenov <abbas.gusenov@pm.me>
2022-08-09 23:48:31 +0600

    Refactoring

--- publi.sh
+++ publi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git add . && git commit -m "Refactoring" \
-  && git tag v0.26.1 -m '0.26.1 tag'
+  && git tag v0.26.2 -m '0.26.2 tag'
 git push --force --tags origin master:master
 rm -rf dist && python3 setup.py bdist_wheel && twine upload -r testpypi dist/*
 pip3 uninstall -y manu
