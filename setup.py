373a7e0241f4f2ceb8f8878db18d7b7a243ee363
Abbas Gussenov <abbas.gusenov@pm.me>
2022-08-09 17:57:14 +0600

    Create package

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+  name='manu',
+  packages=['manu'],
+  version='0.4.0',
+  description='Video Game Software',
+  license='MIT',
+  url='https://github.com/ab-g/Manu-Python',
+  download_url='https://github.com/ab-g/Manu-Python/archive/v0.4.0.tar.gz',
+  keywords='games',
+  classifiers=['Topic :: Games/Entertainment']
+)
