33316eddf18bfdb72f363e1d9cf909dd2ebac7db
Abbas Gussenov <abbas.gusenov@pm.me>
2022-08-09 19:06:01 +0600

    Add function get_scene_data

--- manu/fn.py
+++ manu/fn.py
@@ -7,3 +7,10 @@ def get_resource_pack_data(game_project_dir_path):
     with open(resource_pack_file_path, 'r') as resource_pack_file:
         resource_pack_data = json.load(resource_pack_file)
     return resource_pack_data
+
+
+def get_scene_data(game_project_dir_path, scene_id):
+    scene_file_path = os.path.join(game_project_dir_path, 'scenes/{0}.json'.format(scene_id))
+    with open(scene_file_path, 'r') as scene_file:
+        scene_data = json.load(scene_file)
+    return scene_data
