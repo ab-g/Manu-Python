cbb7e24216d58bd6189c1ffdbbd15618fe192351
Abbas Gussenov <abbas.gusenov@pm.me>
2022-08-09 19:08:25 +0600

    Add function get_character_data

--- manu/fn.py
+++ manu/fn.py
@@ -14,3 +14,13 @@ def get_scene_data(game_project_dir_path, scene_id):
     with open(scene_file_path, 'r') as scene_file:
         scene_data = json.load(scene_file)
     return scene_data
+
+
+def get_character_data(game_project_dir_path, character_id=None):
+    if character_id is None:
+        resource_pack_data = get_resource_pack_data(game_project_dir_path)
+        character_id = resource_pack_data['defaultCharacterId']['uuid']
+    character_file_path = os.path.join(game_project_dir_path, 'characters/{0}.json'.format(character_id))
+    with open(character_file_path, 'r') as character_file:
+        character_data = json.load(character_file)
+    return character_data
--- publi.sh
+++ publi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git add . && git commit -m "Add function get_scene_data" && git tag v0.6.0 -m '0.6.0 tag'
+git add . && git commit -m "Add function get_character_data" && git tag v0.7.0 -m '0.7.0 tag'
 git push --tags origin
 rm -rf dist && python3 setup.py bdist_wheel && twine upload -r testpypi dist/*
 pip3 uninstall -y manu
